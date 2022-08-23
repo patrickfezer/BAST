cdc4d207017989cb16752f1a7dabb929c3c19dcd
Patrick Fezer <p.fezer@gmx.de>
2022-08-23 20:42:52 +0200

    Initial Commit

---
+++ BAST/BASTApp.swift
@@ -0,0 +1,17 @@
+//
+//  BASTApp.swift
+//  BAST
+//
+//  Created by Patrick Fezer on 23.08.22.
+//
+
+import SwiftUI
+
+@main
+struct BASTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BAST/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BAST
+//
+//  Created by Patrick Fezer on 23.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
